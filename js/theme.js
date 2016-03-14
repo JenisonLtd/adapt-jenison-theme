3337281c5eba21a9dd42c90b139430b708498180
Jack Ward <jackward@live.co.uk>
2016-03-14 11:36:37 +0000

    Update and rename vanilla.js to theme.js

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@ define(function(require) {
 	
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
-	var ThemeBlock = require('theme/adapt-contrib-vanilla/js/theme-block');
+	var ThemeBlock = require('theme/adapt-jenison-theme/js/theme-block');
 
 	// Block View
 	// ==========
